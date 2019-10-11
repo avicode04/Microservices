e21f3e8bc81889a811443c17355e788a9b4c9244
avinash <avinashchandrakar04@gmail.com>
2019-10-11 18:01:55 +0530

    First commit

---
+++ movie-service/src/main/java/com/stackroute/Movie/MovieApplication.java
@@ -0,0 +1,18 @@
+package com.stackroute.Movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MovieApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieApplication.class, args);
+	}
+
+}
---
+++ springboot-zool-gateway-proxy/src/main/java/com/stackroute/springbootzoolgatewayproxy/SpringbootZoolGatewayProxyApplication.java
@@ -0,0 +1,39 @@
+package com.stackroute.springbootzoolgatewayproxy;
+
+import com.netflix.zuul.exception.ZuulException;
+import com.stackroute.springbootzoolgatewayproxy.filters.ErrorFilter;
+import com.stackroute.springbootzoolgatewayproxy.filters.PostFilter;
+import com.stackroute.springbootzoolgatewayproxy.filters.PreFilter;
+import com.stackroute.springbootzoolgatewayproxy.filters.RouteFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class SpringbootZoolGatewayProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootZoolGatewayProxyApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+}
