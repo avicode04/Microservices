973c61439c22650a0e46f24eb5e870402153bad4
avinash <avinashchandrakar04@gmail.com>
2019-10-11 20:46:55 +0530

    Second commit

--- springboot-zool-gateway-proxy/src/main/java/com/stackroute/springbootzoolgatewayproxy/SpringbootZoolGatewayProxyApplication.java
+++ springboot-zool-gateway-proxy/src/main/java/com/stackroute/springbootzoolgatewayproxy/SpringbootZoolGatewayProxyApplication.java
@@ -7,13 +7,11 @@ import com.stackroute.springbootzoolgatewayproxy.filters.PreFilter;
 import com.stackroute.springbootzoolgatewayproxy.filters.RouteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
 public class SpringbootZoolGatewayProxyApplication {
 
 	public static void main(String[] args) {
